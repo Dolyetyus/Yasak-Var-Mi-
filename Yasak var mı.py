398be35276312661e1ecdc2467859354a3ea2b76
Musab <67073431+Dolyetyus@users.noreply.github.com>
2020-12-01 23:28:20 +0300

    Update Yasak var mı.py

--- Yasak var mı.py
+++ Yasak var mı.py
@@ -11,33 +11,18 @@ yaş = int(input("Kaç Yaşındasınız: "))
 zaman = datetime.datetime.now()
 saat = zaman.hour
 gün = zaman.weekday()
-app = 1
 
-while app == 1:
-    if(gün>4): 
-        if(saat >= 10 and saat < 20):
-            if(yaş > 20 and yaş < 65):
-                print(Fore.GREEN+Style.BRIGHT+"Dışarı Çıkabilirsiniz")
-                time.sleep(5)
-                sys.exit()
-            elif(yaş >= 65 and saat < 13):
-                print(Fore.GREEN+Style.BRIGHT+"Dışarı Çıkabilirsin")
-                time.sleep(5)
-                sys.exit()
-            elif(yaş <= 20 and saat >= 13 and saat < 16):
-                print(Fore.GREEN+Style.BRIGHT+"Dışarı Çıkabilirsin")
-                time.sleep(5)
-                sys.exit()
-                
-        print(Fore.RED+Style.BRIGHT+"Dışarı Çıkman Yasak")
-        time.sleep(5)
-        sys.exit()
+if(gün>=4 and saat>=21):
+    print(Fore.RED+Style.BRIGHT+"Dışarı Çıkman Yasak")
+    time.sleep(5)
+    sys.exit()
 
-    else: 
+else: 
+    if(gün<4 and saat<21 and saat>5):
         iş = input("Çalışıyor musunuz(E/H): ")
         iş=iş.upper()
-        if (iş == "E" or iş== "EVET"):
-            print(Fore.BLUE+Style.BRIGHT+"İşine gidebilirsin")
+        if (iş=="E" or iş=="EVET"):
+            print(Fore.GREEN+Style.BRIGHT+"Dışarı Çıkabilirsin")
             time.sleep(5)
             sys.exit()
         elif (iş == "H" or iş == "HAYIR"):
@@ -53,7 +38,7 @@ while app == 1:
                 print(Fore.GREEN+Style.BRIGHT+"Dışarı Çıkabilirsin")
                 time.sleep(5)
                 sys.exit()
-
-        print(Fore.RED+Style.BRIGHT+"Dışarı Çıkman Yasak")
-        time.sleep(5)
-        sys.exit()
+        else:
+            print(Fore.RED+Style.BRIGHT+"Dışarı Çıkman Yasak")
+            time.sleep(5)
+            sys.exit()
